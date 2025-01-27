e809df2fa6e2be7400b2bc23f6f879d6f6403798
chinazes532 <chernikof50@gmail.com>
2025-01-27 18:15:34 +0300

    update in models.py

--- app/database/models.py
+++ app/database/models.py
@@ -1,3 +1,5 @@
+from typing import Annotated
+
 from sqlalchemy import ForeignKey, String, BigInteger
 from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase, relationship
 from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
@@ -9,6 +11,8 @@ engine = create_async_engine(url=DB_URL,
 
 async_session = async_sessionmaker(engine)
 
+intpk = Annotated[int, mapped_column(primary_key=True)]
+
 
 class Base(AsyncAttrs, DeclarativeBase):
     pass
@@ -17,7 +21,7 @@ class Base(AsyncAttrs, DeclarativeBase):
 class User(Base):
     __tablename__ = 'users'
 
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[intpk]
     tg_id: Mapped[int] = mapped_column(BigInteger)
     first_name: Mapped[str]
     date: Mapped[str]
